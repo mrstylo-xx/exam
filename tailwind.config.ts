f8fa2f79fc42dd2bdd508b525d7e1d8792f83b67
mrstylo-xx <dearwap.tk@gmail.com>
2026-02-06 14:04:37 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,13 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
         poppins: "var(--font-poppins, 'Poppins', sans-serif)",
         bengali: "var(--font-bengali, 'Noto Serif Bengali', serif)",
-      },
-      colors: {
-        primary: "#4f46e5",
-        secondary: "#7c3aed",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
